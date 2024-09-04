b949b13a5bcac2a706518da88074099fdbac68f3
Mohammed Hussain <mohammed.dvlpr@gmail.com>
2024-09-04 16:45:20 +0530

    adding all required products

---
+++ lib/models/my_product.dart
@@ -0,0 +1,122 @@
+import 'package:e_commerce_app/models/product.dart';
+
+class MyProduct {
+  static List<Product> allProduct = [
+    Product(
+        id: 1,
+        name: "Nike Air 1",
+        price: 181.00,
+        image: "assets/show_images/show_image_1.png",
+        category: "Trending",
+        desciption: "Nike Air 1 is a popular shoe model.",
+        quantity: 1),
+    Product(
+        id: 1,
+        name: "Nike Air 1",
+        price: 120.00,
+        image: "assets/show_images/show_image_2.png",
+        category: "Out of Stock",
+        desciption: "Nike Air 1 is a popular shoe model.",
+        quantity: 1),
+    Product(
+        id: 1,
+        name: "Leather Jacket",
+        price: 100.00,
+        image: "assets/jackets/1.webp",
+        category: "Trending",
+        desciption: "Nike Air 1 is a popular shoe model.",
+        quantity: 1),
+    Product(
+        id: 1,
+        name: "Brown Leather Jacket",
+        price: 100.00,
+        image: "assets/jackets/3.webp",
+        category: "Trending",
+        desciption: "Nike Air 1 is a popular shoe model.",
+        quantity: 1)
+  ];
+  static List<Product> sneackersList = [
+    Product(
+        id: 1,
+        name: "Nike Air 1",
+        price: 181.00,
+        image: "assets/show_images/show_image_1.png",
+        category: "Trending",
+        desciption: "Nike Air 1 is a popular shoe model.",
+        quantity: 1),
+    Product(
+        id: 1,
+        name: "Nike Air 2",
+        price: 50.00,
+        image: "assets/show_images/show_image_2.png",
+        category: "Trending",
+        desciption: "Nike Air 1 is a popular shoe model.",
+        quantity: 1),
+    Product(
+        id: 1,
+        name: "Nike Air 3",
+        price: 101.00,
+        image: "assets/show_images/show_image_3.png",
+        category: "Trending",
+        desciption: "Nike Air 1 is a popular shoe model.",
+        quantity: 1),
+    Product(
+        id: 1,
+        name: "Nike Air 4",
+        price: 171.00,
+        image: "assets/show_images/show_image_4.png",
+        category: "Trending",
+        desciption: "Nike Air 1 is a popular shoe model.",
+        quantity: 1),
+    Product(
+        id: 1,
+        name: "Nike Air 5",
+        price: 120.00,
+        image: "assets/show_images/show_image_5.png",
+        category: "Trending",
+        desciption: "Nike Air 1 is a popular shoe model.",
+        quantity: 1),
+    Product(
+        id: 1,
+        name: "Nike Air 6",
+        price: 161.00,
+        image: "assets/show_images/show_image_6.png",
+        category: "Trending",
+        desciption: "Nike Air 1 is a popular shoe model.",
+        quantity: 1),
+  ];
+  static List<Product> jacketsList = [
+    Product(
+        id: 1,
+        name: "Leather Jacket",
+        price: 100.00,
+        image: "assets/jackets/1.webp",
+        category: "Trending",
+        desciption: "Nike Air 1 is a popular shoe model.",
+        quantity: 1),
+    Product(
+        id: 1,
+        name: "Brown Leather Jacket",
+        price: 100.00,
+        image: "assets/jackets/3.webp",
+        category: "Trending",
+        desciption: "Nike Air 1 is a popular shoe model.",
+        quantity: 1),
+    Product(
+        id: 1,
+        name: "Leather Jacket",
+        price: 100.00,
+        image: "assets/jackets/1.webp",
+        category: "Trending",
+        desciption: "Nike Air 1 is a popular shoe model.",
+        quantity: 1),
+    Product(
+        id: 1,
+        name: "Brown Leather Jacket",
+        price: 100.00,
+        image: "assets/jackets/3.webp",
+        category: "Trending",
+        desciption: "Nike Air 1 is a popular shoe model.",
+        quantity: 1)
+  ];
+}
