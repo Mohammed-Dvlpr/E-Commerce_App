1af90fbcfc25b4c572893b26edea56a30d1e91db
Mohammed Hussain <mohammed.dvlpr@gmail.com>
2024-09-04 16:06:50 +0530

    Adding a button and text

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -8,8 +8,41 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int isSelected = 0;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(children: [
+          const Text(
+            "Our Products",
+            style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              _buildProductCategory(index: 0, name: "All Products"),
+              _buildProductCategory(index: 1, name: "Jackrts"),
+              _buildProductCategory(index: 2, name: "Sneakers"),
+            ],
+          ),
+          Image.asset("assets/show_images/show_image_1.png")
+        ]));
   }
+
+  _buildProductCategory({required int index, required String name}) =>
+      Container(
+        height: 40,
+        width: 100,
+        margin: const EdgeInsets.only(top: 10, right: 10),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: isSelected == index ? Colors.red : Colors.red.shade300,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Text(
+          name,
+          style: const TextStyle(color: Colors.white),
+        ),
+      );
 }
